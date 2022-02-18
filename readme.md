1b622842154ebfe865c5982c7152c1ad86e5b2d8
vorotamoroz <vrtmrz@gmail.com>
2022-02-18 18:25:54 +0900

    many issues fixed, readme wrote. multiple vault sync implemented.

---
+++ readme.md
@@ -0,0 +1,187 @@
+# LiveSync classroom
+
+![screenshot](./images/livesync-classroom-capture.gif)
+
+## What is this?
+
+This is the custom replicator of Self-hosted LiveSync's remote vault to share Obsidian's vault or backup.
+
+It can synchronize documents between vaults,
+having different structures and different passphrases.
+
+And, you can synchronize documents under the specified folder,
+to another vault's specified one.
+
+Of course, ~~bi-~~multi-directional!
+
+# How to use
+## Simply run
+
+1. Clone the GitHub Repository
+```git
+git clone https://github.com/vrtmrz/livesync-classroom.git
+```
+2. Open the config file dat/config.sample.json, edit and save to dat/config.json
+
+3. Simply run as like this.
+```bash
+$ npm i -D
+$ npm run dev
+```
+
+## Docker Instructions
+
+1. Clone the GitHub Repository
+```git
+git clone https://github.com/vrtmrz/livesync-classroom.git
+```
+2. Open the config file dat/config.sample.json, edit and save to dat/config.json
+3. Build the Dockerfile
+```bash
+sudo docker build -t obsidian-livesync-classroom .
+```
+4. Create the docker container
+```bash
+sudo docker-compose up -d
+```
+
+# Configuration
+
+The configuration file consists of the following structure.
+
+```json
+{
+    // "config_1" is just the name for identifying the connection.
+    "config_1": {
+        // "private" and "shared" don't mean which is primary or not.
+        // They are even. But I made so many misconfigurations while debugging
+        // with the names "database_a" and "database_b",
+        // so it was just named as like this.
+        "private": { 
+            "uri": "http://localhost:5984/private1_vault",
+            "auth": {
+                "username": "username_of_private_vault",
+                "password": "password_of_private_vault",
+                "passphrase": "passphrase_of_private_vault"
+            },
+            "path": "shared/" // Shared folder
+        },
+        "shared": {
+            "uri": "http://localhost:5984/shared_vault",
+            "auth": {
+                "username": "username_of_shared_vault",
+                "password": "password_of_shared_vault",
+                "passphrase": "passphrase_of_shared_vault"
+            },
+            "path": ""
+        }
+    },
+    // You can set up multiple vault syncing.
+    "config_2": {
+        "private": {
+            "uri": "http://localhost:5984/private2_vault",
+            "auth": {
+                "username": "username_of_private_vault",
+                "password": "password_of_private_vault",
+                "passphrase": "passphrase_of_private_vault"
+            },
+            "path": "shared/"
+        },
+        "shared": {
+            "uri": "http://localhost:5984/shared_vault",
+            "auth": {
+                "username": "username_of_shared_vault",
+                "password": "password_of_shared_vault",
+                "passphrase": "passphrase_of_shared_vault"
+            },
+            "path": ""
+        }
+    }
+}
+```
+## Realistic example
+
+| name                       | database_uri                              | CouchDB username | CouchDB password | vault E2EE passphrase | sharing path |
+| -------------------------- | ----------------------------------------- | ---------------- | ---------------- | --------------------- | ------------ |
+| private vault of Cornbread | http://localhost:5984/classroom_cornbread | cornbread        | tackle           | glucose               | shared/      |
+| shared vault               | http://localhost:5984/classroom_shared    | common_user      | resu_nommoc      | cocoa                 |              |
+| private vault of Vanilla   | http://localhost:5984/classroom_vanilla   | vanilla          | liberty          | smock                 | kyouyuu/     |
+
+Cornbread's every document under "shared" synchronized with top of the shared vault:
+
+| Cornbread          | shared            |
+| ------------------ | ----------------- |
+| document1          | *Not transferred* |
+| document2          | *Not transferred* |
+| shared/shared_doc1 | shared_doc1       |
+| shared/sub/sub_doc | sub/sub_doc       |
+
+Vanilla's every document under "kyouyuu" synchronized with top of the shared vault:
+
+| Vanilla                  | shared            |
+| ------------------------ | ----------------- |
+| documentA                | *Not transferred* |
+| documentB                | *Not transferred* |
+| kyouyuu/some_doc         | some_doc          |
+| kyouyuu/sub/some_sub_doc | sub/some_sub_doc  |
+
+Totally synchronized like this:
+
+| Cornbread               | shared            | Vanilla                  |
+| ----------------------- | ----------------- | ------------------------ |
+| document1               | *Not transferred* |                          |
+| document2               | *Not transferred* |                          |
+|                         | *Not transferred* | documentA                |
+|                         | *Not transferred* | documentB                |
+| shared/shared_doc1      | shared_doc1       | kyouyuu/shared_doc1      |
+| shared/some_doc         | some_doc          | kyouyuu/some_doc         |
+| shared/sub/some_sub_doc | sub/some_sub_doc  | kyouyuu/sub/some_sub_doc |
+| shared/sub/sub_doc      | sub/sub_doc       | kyouyuu/sub/sub_doc      |
+
+... with the configuration below:
+
+```json
+{
+    "cornbread_to_shared": {
+        "private": {
+            "uri": "http://localhost:5984/classroom_cornbread",
+            "auth": {
+                "username": "cornbread",
+                "password": "tackle",
+                "passphrase": "glucose"
+            },
+            "path": "shared/"
+        },
+        "shared": {
+            "uri": "http://localhost:5984/classroom_shared",
+            "auth": {
+                "username": "common_user",
+                "password": "resu_nommoc",
+                "passphrase": "cocoa"
+            },
+            "path": ""
+        }
+    },
+    "vanilla_to_shared": {
+        "private": {
+            "uri": "http://localhost:5984/classroom_vanilla",
+            "auth": {
+                "username": "vanilla",
+                "password": "liberty",
+                "passphrase": "smock"
+            },
+            "path": "kyouyuu/"
+        },
+        "shared": {
+            "uri": "http://localhost:5984/classroom_shared",
+            "auth": {
+                "username": "common_user",
+                "password": "resu_nommoc",
+                "passphrase": "cocoa"
+            },
+            "path": ""
+        }
+    }
+}
+
+```
--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,42 @@ import { decrypt, encrypt } from "./e2ee.js";
 import { PouchDB as PouchDB_src } from "./pouchdb.js";
 import * as fs from "fs";
 import { Logger } from "./logger.js";
-import { LOG_LEVEL } from "./types.js";
+import { configFile, eachConf, LOG_LEVEL, type_direction } from "./types.js";
+
 const xxhash = require("xxhash-wasm");
 const PouchDB: PouchDB.Static<{}> = PouchDB_src;
+
+const statFile = "./dat/stat.json";
+const direction_to_disp: { [key: string]: string } = {
+    "-->": "private-->shared",
+    "<--": "shared-->private",
+};
+
+let running: { [key: string]: boolean } = {};
+
+let h32Raw: (inputBuffer: Uint8Array, seed?: number | undefined) => number;
+let h32: (input: string, seed?: number) => string;
+let known_files: string[] = [];
+let syncStat: { [key: string]: { private_to_shared: string; shared_to_private: string } };
+let saveStatTimer: NodeJS.Timeout | undefined = undefined;
+let saveStatCount: 0;
+
+function addKnownFile(syncKey: string, direction: type_direction, id: string, rev: string) {
+    known_files.push(`${syncKey}-${direction}-${id}-${rev}`);
+    known_files = known_files.slice(-50);
+}
+function isKnownFile(syncKey: string, direction: type_direction, id: string, rev: string) {
+    return known_files.indexOf(`${syncKey}-${direction}-${id}-${rev}`) !== -1;
+}
 
 function log(log: any) {
     Logger(log, LOG_LEVEL.INFO);
 }
 
-let h32Raw: (inputBuffer: Uint8Array, seed?: number | undefined) => number;
-let h32: (input: string, seed?: number) => string;
-let known_files: string[] = [];
-type type_direction = "-->" | "<--";
+function delay(ms: number): Promise<void> {
+    return new Promise((res) => setTimeout(() => res(), ms));
+}
 
-function addKnownFile(direction: type_direction, id: string, rev: string) {
-    known_files.push(`${direction}-${id}-${rev}`);
-    known_files = known_files.slice(-50);
-}
-function isKnownFile(direction: type_direction, id: string, rev: string) {
-    return known_files.indexOf(`${direction}-${id}-${rev}`) !== -1;
-}
-const statFile = "./dat/stat.json";
-let syncStat = { main_to_shared: "now", shared_to_main: "now" };
-let saveStatTimer: NodeJS.Timeout | undefined = undefined;
-let saveStatCount: 0;
 function saveStat() {
     fs.writeFileSync(statFile, JSON.stringify(syncStat));
 }
@@ -43,37 +55,50 @@ function triggerSaveStat() {
         }, 500);
     }
 }
-interface config {
-    uri: string;
-    auth: {
-        username: string;
-        password: string;
-        passphrase: string;
-    };
-    path: string;
-}
-interface configFile {
-    private: config;
-    shared: config;
+interface directionAndType {
+    syncKey: string;
+    direction_on_stat: "private_to_shared" | "shared_to_private";
+    direction: type_direction;
+    fromDB: PouchDB.Database;
+    fromPrefix: string;
+    toDB: PouchDB.Database;
+    toPrefix: string;
+    decryptKey: string;
+    encryptKey: string;
 }
 
 async function main() {
+    log("LiveSync-classroom starting up.");
     let xx = await xxhash();
+    h32Raw = xx.h32Raw;
+    h32 = xx.h32ToString;
     let config: configFile = JSON.parse(fs.readFileSync("./dat/config.json") + "");
+    //let procs = [];
+    try {
+        syncStat = JSON.parse(fs.readFileSync(statFile) + "");
+    } catch (ex) {
+        log("could not read pervious sync status, initialized.");
+        syncStat = {};
+    }
+    let procs = Object.entries(config).map((e) => eachProc(e[0], e[1]));
+    await Promise.allSettled(procs);
+}
+async function eachProc(syncKey: string, config: eachConf) {
+    log(`${syncKey} started`);
 
-    const mainDB = config.private.uri;
-    const mainAuth = config.private.auth;
-    const mainPath = config.private.path;
+    const privateDB = config.private.uri;
+    const privateAuth = config.private.auth;
+    const privatePath = config.private.path;
 
     const sharedDB = config.shared.uri;
     const sharedAuth = config.shared.auth;
     const sharedPath = config.shared.path;
 
-    const main_remote = new PouchDB(mainDB, { auth: mainAuth });
+    const private_remote = new PouchDB(privateDB, { auth: privateAuth });
     const shared_remote = new PouchDB(sharedDB, { auth: sharedAuth });
 
     async function sanityCheck() {
-        let mr = await main_remote.info();
+        let mr = await private_remote.info();
         log("Main Remote Database");
         log(mr);
 
@@ -81,94 +106,97 @@ async function main() {
         log("Shared Remote Database");
         log(sr);
     }
-    try {
-        var st = JSON.parse(fs.readFileSync(statFile) + "");
-        syncStat.main_to_shared == st.main_to_shared ?? "now";
-        syncStat.shared_to_main == st.shared_to_main ?? "now";
-    } catch (ex) {
-        log("could not read pervious sync status, initialized.");
+
+    if (!(syncKey in syncStat)) {
+        syncStat[syncKey] = {
+            private_to_shared: "now",
+            shared_to_private: "now",
+        };
     }
-    h32Raw = xx.h32Raw;
-    h32 = xx.h32ToString;
-    log("LiveSync-classroom starting up.");
+
     try {
         await sanityCheck();
     } catch (ex) {
         log("Error on checking database");
         log(ex);
+        process.exit(-1);
     }
-    log("Start main watching");
-    main_remote
-        .changes({
-            live: true,
-            include_docs: true,
-            style: "all_docs",
-            since: syncStat.main_to_shared,
-            filter: (doc, params) => {
-                if (doc._id.startsWith(mainPath) && isVaildDoc(doc._id)) {
-                    return true;
-                }
-                return false;
-            },
-        })
-        .on("change", async function (change) {
-            if (change.doc?._id.startsWith(mainPath) && isVaildDoc(change.doc._id)) {
-                let x = await transferDoc("-->", main_remote, change.doc, mainPath, shared_remote, sharedPath, mainAuth.passphrase, sharedAuth.passphrase);
-                if (x) {
-                    syncStat.main_to_shared = change.seq + "";
-                    triggerSaveStat();
-                }
-            }
-        })
-        .on("error", function (err) {
-            log("error");
-            log(err);
-            process.exit(-1);
-        });
-    shared_remote
-        .changes({
-            live: true,
-            include_docs: true,
-            style: "all_docs",
-            since: syncStat.shared_to_main,
-            filter: (doc, params) => {
-                if (doc._id.startsWith(sharedPath) && isVaildDoc(doc._id)) {
-                    return true;
-                }
-                return false;
-            },
-        })
-        .on("change", async function (change) {
-            if (change.doc?._id.startsWith(sharedPath) && isVaildDoc(change.doc._id)) {
-                let x = await transferDoc("<--", shared_remote, change.doc, sharedPath, main_remote, mainPath, sharedAuth.passphrase, mainAuth.passphrase);
-                if (x) {
-                    syncStat.shared_to_main = change.seq + "";
-                    triggerSaveStat();
-                }
-            }
-        })
-        .on("error", function (err) {
-            log("error");
-            log(err);
-            process.exit(-1);
-        });
+    log("Start watching");
+
+    let pairs: directionAndType[] = [
+        {
+            syncKey: syncKey,
+            direction_on_stat: "private_to_shared",
+            direction: "-->",
+            fromDB: private_remote,
+            fromPrefix: privatePath,
+            toDB: shared_remote,
+            toPrefix: sharedPath,
+            decryptKey: privateAuth.passphrase,
+            encryptKey: sharedAuth.passphrase,
+        },
+        {
+            syncKey: syncKey,
+            direction_on_stat: "shared_to_private",
+            direction: "<--",
+            toDB: private_remote,
+            toPrefix: privatePath,
+            fromDB: shared_remote,
+            fromPrefix: sharedPath,
+            encryptKey: privateAuth.passphrase,
+            decryptKey: sharedAuth.passphrase,
+        },
+    ];
+    try {
+        let results = await Promise.allSettled(
+            pairs.map(
+                (e) =>
+                    new Promise((res, rej) => {
+                        e.fromDB
+                            .changes({
+                                live: true,
+                                include_docs: true,
+                                style: "all_docs",
+                                since: syncStat[syncKey][e.direction_on_stat],
+                                filter: (doc, _) => {
+                                    return doc._id.startsWith(e.fromPrefix) && isVaildDoc(doc._id);
+                                },
+                            })
+                            .on("change", async function (change) {
+                                if (change.doc?._id.startsWith(e.fromPrefix) && isVaildDoc(change.doc._id)) {
+                                    let x = await transferDoc(e.syncKey, e.direction, e.fromDB, change.doc, e.fromPrefix, e.toDB, e.toPrefix, e.decryptKey, e.encryptKey);
+                                    if (x) {
+                                        syncStat[syncKey][e.direction_on_stat] = change.seq + "";
+                                        triggerSaveStat();
+                                    }
+                                }
+                            })
+                            .on("error", function (err) {
+                                rej(err);
+                            })
+                            .on("complete", (result) => {
+                                res(result);
+                            });
+                    })
+            )
+        );
+        console.dir(results);
+    } catch (ex) {
+        log("error");
+        log(ex);
+    }
 }
 
-function delay(ms: number): Promise<void> {
-    return new Promise((res) => setTimeout(() => res(), ms));
-}
 async function getChildren(children: string[], db: PouchDB.Database) {
     let items = await db.allDocs({ include_docs: true, keys: [...children] });
-    let entries = items.rows.map((e) => e.doc);
-    return entries;
+    return items.rows.map((e) => e.doc);
 }
 function normalizeDocForDiff(doc: any): string {
-    const r = JSON.stringify({
+    return JSON.stringify({
         mtime: doc.mtime ?? "",
-        chilldren: doc.children ?? [],
+        children: doc.children ?? [],
         _deleted: doc._deleted,
     });
-    return r;
 }
 function isVaildDoc(id: string): boolean {
     if (id == "obsydian_livesync_version") return false;
@@ -179,8 +207,9 @@ function getCounterSide(direction: type_direction): type_direction {
     if (direction == "-->") return "<--";
     return "-->";
 }
-let running = false;
+
 async function transferDoc(
+    syncKey: string,
     direction: type_direction,
     fromDB: PouchDB.Database,
     fromDoc: PouchDB.Core.ExistingDocument<PouchDB.Core.ChangesMeta>,
@@ -190,54 +219,54 @@ async function transferDoc(
     decryptKey: string,
     encryptKey: string
 ): Promise<boolean> {
-    const docKey = `${direction} ${fromDoc._id} (${fromDoc._rev})`;
-    while (running) {
+    const docKey = `${syncKey}:${direction_to_disp[direction]} ${fromDoc._id} (${fromDoc._rev})`;
+    while (running[syncKey]) {
         await delay(100);
     }
     try {
-        running = true;
-        if (isKnownFile(direction, fromDoc._id, fromDoc._rev)) {
+        running[syncKey] = true;
+        if (isKnownFile(syncKey, direction, fromDoc._id, fromDoc._rev)) {
             return true;
         }
         log(`doc:${docKey} begin Transfer`);
-        let continue_count = 10;
+        let continue_count = 3;
         try {
-            let transferDoc: PouchDB.Core.ExistingDocument<PouchDB.Core.ChangesMeta> & { children?: string[] } = { ...fromDoc, _id: toPrefix + fromDoc._id.substring(fromPrefix.length) };
+            let sendDoc: PouchDB.Core.ExistingDocument<PouchDB.Core.ChangesMeta> & { children?: string[] } = { ...fromDoc, _id: toPrefix + fromDoc._id.substring(fromPrefix.length) };
             let retry = false;
             const userpasswordHash = h32Raw(new TextEncoder().encode(encryptKey));
             do {
                 if (retry) {
                     continue_count--;
                     if (continue_count == 0) {
-                        log(`doc:${docKey} retry count exeeedec`);
+                        log(`doc:${docKey} retry failed`);
                         return false;
                     }
                     await delay(1500);
                 }
                 retry = false;
-                let oldRemoteDoc: any;
+                let oldRemoteDoc: any = { children: [] };
                 try {
-                    oldRemoteDoc = (await toDB.get(transferDoc._id)) as any;
-                    transferDoc._rev = oldRemoteDoc._rev;
+                    oldRemoteDoc = (await toDB.get(sendDoc._id)) as any;
+                    sendDoc._rev = oldRemoteDoc._rev;
                 } catch (ex: any) {
                     if (ex.status && ex.status == 404) {
-                        if (transferDoc._deleted) {
+                        if (sendDoc._deleted) {
                             // we have to skip this.
                             log(`doc:${docKey} it had been deleted, and there's no need to synchronized`);
                             return true;
                         }
-                        // If new doc. we doesn't need _rev;
-                        delete (transferDoc as any)._rev;
+                        // If new doc. we don't need _rev;
+                        delete (sendDoc as any)._rev;
                     } else {
                         throw ex;
                     }
                 }
 
-                if (!transferDoc.children) {
+                if (!sendDoc.children) {
                     log(`doc:${docKey}: Warning! document doesn't have chunks, skipped`);
                     return false;
                 }
-                let cx = transferDoc.children;
+                let cx = sendDoc.children;
                 let children = await getChildren(cx, fromDB);
 
                 if (children.includes(undefined)) {
@@ -279,9 +308,9 @@ async function transferDoc(
                         retry = true;
                         continue;
                     }
-                    transferDoc.children = encrypted_children.map((e) => e._id);
+                    sendDoc.children = encrypted_children.map((e) => e._id);
                     let diffDoc = encrypted_children.filter((e) => oldRemoteDoc.children.indexOf(e._id) === -1);
-                    log(`doc:${docKey}:Transfering ${diffDoc.length} chunk(s)`);
+                    log(`doc:${docKey}:Transferring ${diffDoc.length} chunk(s)`);
                     let stat = await toDB.bulkDocs([...(diffDoc as PouchDB.Core.ExistingDocument<PouchDB.Core.AllDocsMeta>[])]);
 
                     if (stat.find((e) => (e as any).status && (e as any).status != 409)) {
@@ -292,30 +321,30 @@ async function transferDoc(
                     }
 
                     try {
-                        if (normalizeDocForDiff(oldRemoteDoc) == normalizeDocForDiff(transferDoc)) {
+                        if (normalizeDocForDiff(oldRemoteDoc) == normalizeDocForDiff(sendDoc)) {
                             log(`doc:${docKey}: synchronized`);
                             retry = false;
                             return true;
                         }
-                        transferDoc._rev = oldRemoteDoc._rev;
+                        sendDoc._rev = oldRemoteDoc._rev;
                     } catch (ex: any) {
                         if (ex.status && ex.status == 404) {
-                            if (transferDoc._deleted) {
+                            if (sendDoc._deleted) {
                                 // we have to skip this.
                                 log(`doc:${docKey} it had been deleted, and there's no need to synchronized`);
                                 return true;
                             }
-                            // If new doc. we doesn't need _rev;
-                            delete (transferDoc as any)._rev;
+                            // If new doc. we don't need _rev;
+                            delete (sendDoc as any)._rev;
                         } else {
                             throw ex;
                         }
                     }
                     try {
-                        let e = await toDB.put(transferDoc);
+                        let e = await toDB.put(sendDoc);
                         if (e.ok) {
-                            log(`doc:${docKey} transfered`);
-                            addKnownFile(getCounterSide(direction), e.id, e.rev);
+                            log(`doc:${docKey} transferred`);
+                            addKnownFile(syncKey, getCounterSide(direction), e.id, e.rev);
                             return true;
                         } else {
                             log(`doc:${docKey} failed to transfer`);
@@ -337,7 +366,7 @@ async function transferDoc(
             log(ex);
         }
     } finally {
-        running = false;
+        running[syncKey] = false;
     }
     return false;
 }
